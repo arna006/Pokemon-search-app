58eeafc9d1f99445a260e4012adf2760216dbce4
Ari Naqid <arinaqid@gmail.com>
2025-05-22 10:30:24 +0200

    Update server.js
    Fixed some bugs in my code so far....

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const { MongoClient } = require('mongodb');
 
@@ -8,51 +7,41 @@ app.use(express.json());
 const uri = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(uri);
 
-
-async function mongodb(){
-    await client.connect();
-    const db = client.db('test');
-    console.log('Connected to MongoDB');
-
+async function mongodb() {
+  await client.connect();
+  const db = client.db('test');
+  console.log('Connected to MongoDB');
 }
-
 mongodb();
 
+// REGISTER
+ // 1. Hent username og password fra req.body
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const db = client.db('pokemonApp');
+  const users = db.collection('users');
+  await users.insertOne({ username, password });
+  res.send('POST request received');
+});
 
-app.post('/register', async (req, res) =>{
+// LOGIN
+ // 2. Sjekk om brukeren finnes i databasen
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const db = client.db('pokemonApp');
+  const users = db.collection('users');
+  const user = await users.findOne({ username });
 
-    // 1. Hent username og password fra req.body
-    const {username, password} = req.body;
+    // 3. Sjekk passord og send bekreftelse
 
-    // 2. Sett det inn i databasen
-    const db = client.db('pokemonApp');
-    const users = db.collection('users');
-    await users.insertOne({username, password});
+  if (user && user.password === password) {
+    res.send('Login successful');
+  } else {
+    res.status(401).send('Invalid username or password');
+  }
+});
 
-    // 3. Send tilbake en bekreftelse
-    res.send('POST request received');
-
-})
-
+// START SERVER (skal alltid ligge til slutt!)
 app.listen(3000, () => {
-    console.log('Serveren kjører på http://localhost:3000');
-  });
-
-
-    app.post('/login', async (req, res) => {
-        // 1. Hent username og password fra req.body
-        const { username, password } = req.body;
-
-        // 2. Sjekk om brukeren finnes i databasen
-        const db = client.db('pokemonApp');
-        const users = db.collection('users');
-        const user = await users.findOne({ username });
-
-        // 3. Sjekk passord og send bekreftelse
-        if (user && user.password === password) {
-          res.send('Login successful');
-        } else {
-          res.status(401).send('Invalid username or password');
-        }
-        }
-    );
+  console.log('Serveren kjører på http://localhost:3000');
+});
