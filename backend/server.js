fdf6f107558f4658ba1c4641b1a8fe80ce2eaea0
Ari Naqid <arinaqid@gmail.com>
2025-05-22 10:58:39 +0200

    Update server.js
    - Bygget en Express-server
     - Knyttet til MongoDB
    - Lagt til bruker med /register
    -  Logget inn med /login
    - Testet det profesjonelt i Postman
    -Feilsøkt og fikset – steg for steg

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const { MongoClient } = require('mongodb');
 const app = express();
 app.use(express.json());
 
+// 0. Koble til MongoDB
 const uri = 'mongodb://127.0.0.1:27017';
 const client = new MongoClient(uri);
 
@@ -15,25 +16,30 @@ async function mongodb() {
 mongodb();
 
 // REGISTER
- // 1. Hent username og password fra req.body
 app.post('/register', async (req, res) => {
+  // 1. Hent username og password fra req.body
   const { username, password } = req.body;
+
+  // 2. Sett det inn i databasen
   const db = client.db('pokemonApp');
   const users = db.collection('users');
   await users.insertOne({ username, password });
+
+  // 3. Send bekreftelse
   res.send('POST request received');
 });
 
 // LOGIN
- // 2. Sjekk om brukeren finnes i databasen
 app.post('/login', async (req, res) => {
+  // 1. Hent username og password fra req.body
   const { username, password } = req.body;
+
+  // 2. Sjekk om brukeren finnes i databasen
   const db = client.db('pokemonApp');
   const users = db.collection('users');
   const user = await users.findOne({ username });
 
-    // 3. Sjekk passord og send bekreftelse
-
+  // 3. Sjekk passord og send bekreftelse
   if (user && user.password === password) {
     res.send('Login successful');
   } else {
@@ -42,6 +48,7 @@ app.post('/login', async (req, res) => {
 });
 
 // START SERVER (skal alltid ligge til slutt!)
-app.listen(3000, () => {
-  console.log('Serveren kjører på http://localhost:3000');
-});
+// START SERVER (alltid nederst!)
+    app.listen(3000, () => {
+    console.log('Serveren kjører på http://localhost:3000');
+  });
